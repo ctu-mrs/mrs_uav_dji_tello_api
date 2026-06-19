2927bc20a00511629e216cf49d76d8a9ea48e4cd
Tomas Baca <Tomas Baca>
2026-06-19 20:03:10 +0200

    Revert "udpated to the new coroutined api"
    This reverts commit f333be03bc7d1da71d84b88317f2155e8ce89edd.

--- src/api.cpp
+++ src/api.cpp
@@ -67,8 +67,8 @@ public:
 
   // | -------------------- service callbacks ------------------- |
 
-  mrs_lib::Task<std::tuple<bool, std::string>> callbackArming(const bool &request);
-  mrs_lib::Task<std::tuple<bool, std::string>> callbackOffboard(void);
+  std::tuple<bool, std::string> callbackArming(const bool &request);
+  std::tuple<bool, std::string> callbackOffboard(void);
 
 private:
   bool is_initialized_ = false;
@@ -140,7 +140,7 @@ void Api::initialize(const rclcpp::Node::SharedPtr &node, std::shared_ptr<mrs_ua
   common_handlers_ = common_handlers;
 
   cbkgrp_subs_ = node_->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
-  cbkgrp_sc_   = node_->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+  cbkgrp_sc_   = node_->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
 
   pos_ << 0, 0, 0;
   last_update_ = rclcpp::Time(0, 0, clock_->get_clock_type());
@@ -382,7 +382,7 @@ void Api::callbackTrackerCmd(const mrs_msgs::msg::TrackerCommand::ConstSharedPtr
 
 /* callbackArming() //{ */
 
-mrs_lib::Task<std::tuple<bool, std::string>> Api::callbackArming([[maybe_unused]] const bool &request) {
+std::tuple<bool, std::string> Api::callbackArming([[maybe_unused]] const bool &request) {
 
   std::stringstream ss;
 
@@ -396,7 +396,7 @@ mrs_lib::Task<std::tuple<bool, std::string>> Api::callbackArming([[maybe_unused]
 
   RCLCPP_INFO(node_->get_logger(), "calling for %s", request ? "arming" : "disarming");
 
-  auto response = co_await sch_arm_.callAwaitable(srv_out);
+  auto response = sch_arm_.callSync(srv_out);
 
   if (response) {
 
@@ -414,14 +414,14 @@ mrs_lib::Task<std::tuple<bool, std::string>> Api::callbackArming([[maybe_unused]
     RCLCPP_ERROR_STREAM_THROTTLE(node_->get_logger(), *clock_, 1000, "" << ss.str());
   }
 
-  co_return {response.value()->success, ss.str()};
+  return {response.value()->success, ss.str()};
 }
 
 //}
 
 /* callbackOffboard() //{ */
 
-mrs_lib::Task<std::tuple<bool, std::string>> Api::callbackOffboard(void) {
+std::tuple<bool, std::string> Api::callbackOffboard(void) {
 
   std::stringstream ss;
 
@@ -430,14 +430,14 @@ mrs_lib::Task<std::tuple<bool, std::string>> Api::callbackOffboard(void) {
     offboard_ = false;
     ss << "not armed";
     RCLCPP_ERROR_THROTTLE(node_->get_logger(), *clock_, 1000, "%s", ss.str().c_str());
-    co_return {false, ss.str()};
+    return {false, ss.str()};
 
   } else {
 
     offboard_ = true;
     ss << "success";
     RCLCPP_ERROR_THROTTLE(node_->get_logger(), *clock_, 1000, "%s", ss.str().c_str());
-    co_return {true, ss.str()};
+    return {true, ss.str()};
   }
 }
 
